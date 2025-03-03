647d3df584099af9c155c3d35c4e801cd4164ef9
jerethom <jeremythomas73@outlook.com>
2025-03-02 19:27:53 -0800

    added cell logic

--- bsg_cgol_cell/v/bsg_cgol_cell.v
+++ bsg_cgol_cell/v/bsg_cgol_cell.v
@@ -27,8 +27,33 @@ module bsg_cgol_cell (
 
   // TODO: Design your bsg_cgl_cell
   // Hint: Find the module to count the number of neighbors from basejump
+  logic current_val, next_val;
+  wire [3:0] count;
   
-  
+  assign data_o = current_val;
+
+//   bsg_thermometer_count #(.width_p(8)) count_adj (.i(data_i), .o(count));
+    bsg_popcount #(.width_p(8)) count_adj (.i(data_i), .o(count));
+
+    always @(posedge clk_i) begin
+        current_val <= next_val;
+    end
+    
+    always_comb begin
+        if (en_i && update_i || !en_i && !update_i) begin
+            next_val = current_val;
+        end
+        else if (en_i) begin
+            if (count == 3 || count == 2 && current_val == 1)
+                next_val = 1;
+            else
+                next_val = 0;
+        end
+        // if (update_i)
+        else begin
+            next_val = update_val_i;
+        end
+    end
 
 
 
